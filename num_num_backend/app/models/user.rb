2791c62b06a4bc77caaca15f8c3f4c9a537d9d5b
supermancbs <clark.sacktor@gmail.com>
2016-09-16 18:51:57 -0400

    added validations

--- num_num_backend/app/models/user.rb
+++ num_num_backend/app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ApplicationRecord
   has_one :customer
   has_one :chef
+  validates :name, presence: true
+  validates :email, presence: true
+  validates :password_digest, presence: true
 end
